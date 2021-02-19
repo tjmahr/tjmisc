01c242e552ff6617280c3495499d7683498438c6
TJ Mahr <mahr@wisc.edu>
2021-02-19 12:54:30 -0600

    document colors

--- R/colors.R
+++ R/colors.R
@@ -1,4 +1,5 @@
-
+#' Colors I like
+#'
 #' @export
 tjm_colors <- list(
   help = "https://davidmathlogic.com/colorblind/",
@@ -8,10 +9,14 @@ tjm_colors <- list(
     # yes friendly
     priors = c("#b0e2ff", "#8b5f65", "#473c8b"),
     ages = c("#440154FF", "#35608DFF", "#22A884FF", "#FDE725FF"),
-    infoboxes = c("#EEF7FA", "#E5EAEF", "#0074D9")
+    infoboxes = c("#EEF7FA", "#E5EAEF", "#0074D9"),
+    etc = c("#009DA1", "#BB655F")
   ),
   # not cvd friendly
   srcld2019 = c("#1FA35C", "#E7552C", "#3B7B9E", "#414145"),
+  # some album art?
+  idk_1 = c("#4A5C50", "#ABAE83", "#CEA9C8", "#D4D3D1", "#F0F0F0", "#FAFAFA"),
+  idk_2 = c("#1f8fc1", "#fed531", "#ff6634", "#ff3131", "#191919"),
   posterior_on_grey = c(
     fill = c("#EF486899", "#F39B4C99", "#ECD99999"),
     fill_legend = c("#ECE0BA", "#F0B778", "#EF746E"),
@@ -19,6 +24,7 @@ tjm_colors <- list(
   ),
   nice = list(
     fiesta_red = "#dd4132",
-    posterior_purple = "#704D9E"
+    posterior_purple = "#704D9E",
+    purple_robe = "#b52d7e"
   )
 )
