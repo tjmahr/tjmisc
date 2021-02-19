96b5efe7fe33d5907c965e4181b5181c4fdbc387
TJ Mahr <mahr@wisc.edu>
2021-02-19 12:38:26 -0600

    keep track of nice colors

---
+++ R/colors.R
@@ -0,0 +1,24 @@
+
+#' @export
+tjm_colors <- list(
+  help = "https://davidmathlogic.com/colorblind/",
+  dissertation = list(
+    # not cvd friendly
+    conditions = c("#6b7a8f", "#f7882f", "#008b8b", "#111111"),
+    # yes friendly
+    priors = c("#b0e2ff", "#8b5f65", "#473c8b"),
+    ages = c("#440154FF", "#35608DFF", "#22A884FF", "#FDE725FF"),
+    infoboxes = c("#EEF7FA", "#E5EAEF", "#0074D9")
+  ),
+  # not cvd friendly
+  srcld2019 = c("#1FA35C", "#E7552C", "#3B7B9E", "#414145"),
+  posterior_on_grey = c(
+    fill = c("#EF486899", "#F39B4C99", "#ECD99999"),
+    fill_legend = c("#ECE0BA", "#F0B778", "#EF746E"),
+    median_line = "#704D9E"
+  ),
+  nice = list(
+    fiesta_red = "#dd4132",
+    posterior_purple = "#704D9E"
+  )
+)
